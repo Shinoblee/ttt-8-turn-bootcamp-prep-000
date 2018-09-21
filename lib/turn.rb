8b2a32d45da14b7e45f8e58f35365ae2bd4dfe4b
Lee Simon <shinoblee@gmail.com>
2018-09-21 16:19:07 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,28 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  input_to_index(user_input)
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input = user_input.to_i
+  if user_input.between?(1,9)
+    user_input -= 1
+  else
+    puts "You must select a valid number:"
+    user_input = gets.strip
+    input_to_index(user_input)
+  end
+end
+
+def valid_move(board, user_input)
+  
+end
