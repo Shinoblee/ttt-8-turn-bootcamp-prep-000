6f90317206db35243793a28cf24c531dcaa8299d
Lee Simon <shinoblee@gmail.com>
2018-09-21 17:03:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    
+    puts "Still Good"
   else
     puts "Please make a valid selection"
     turn(board)
